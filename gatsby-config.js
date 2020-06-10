46455a39f86b67457ccb3c498ee55a72c2ffae9c
Me <me@example.com>
2020-06-10 14:21:29 -0500

    Trying to add plugins now.  Still do not have sidebar showing up yet though.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,10 @@ module.exports = {
 			resolve: 'gatsby-plugin-tinacms',
 			options: {
 				sidebar: {
-					hidden: process.env.NODE_ENV === "production",
+					//hidden: process.env.NODE_ENV === "production",
 					position: "displace"
 				},
+				plugins: ["gatsby-tinacms-git", "gatsby-tinacms-remark", "gatsby-tinacms-json"]
 			}
 		}
 	]
