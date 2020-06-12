45167b854ec8c51e8e6b593560790d7374d5cefe
Me <me@example.com>
2020-06-12 16:57:56 -0500

    Wait, now my mvb is at 100.  Is PageSpeed just working fast today?  Moving to TinaCMS build with production-hidden editor.

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	plugins: [
+		`gatsby-plugin-preact`,
+		`gatsby-plugin-netlify`,
+		{
+			resolve: `gatsby-plugin-tinacms`,
+			options: {
+				sidebar: {
+					hidden: process.env.NODE_ENV === "production",
+					position: "displace"
+				},
+				plugins: [`gatsby-tinacms-git`, `gatsby-tinacms-remark`]
+			}
+		}
+	]
+};
