ef6afc25aa38a4153226a93884f428aedfb3dfb9
Me <me@example.com>
2020-06-14 16:28:58 -0500

    Okay, let's see if I made a compiling take-2 Gatsby site

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,41 @@
-exports.onCreateNode = ({ node, getNode }) => {
-  console.log('Node Internal Type: ' + node.internal.type + '; Node name: ' + node.name)
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { `createNodeField` } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+	const urlSuffixIdea = createFilePath({ node, getNode, basePath: `pages` })
+	createNodeField({
+	  node,
+	  name: `suggestedURLSuffix`
+	  value: urlSuffixIdea,
+	})
+  }
 };
 
 exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const queryResult = await graphql(`
+	query {
+	  allMarkdownRemark {
+		  edges {
+			node {
+			  fields {
+				suggestedURLSuffix
+			  }
+			}
+		  }
+	  }
+	}
+  `)
+  nodes = queryResult.data.allMarkdownRemark.edges
+  nodes.forEach(({ node }) => {
+    createPage({
+	  path: node.fields.suggestedURLSuffix,
+	  component: path.resolve(`./src/templates/${node.frontmatter.template}.js`),
+	  context: {
+	    frontmatter: node.frontmatter,
+	  },
+    })
+  }
 };
