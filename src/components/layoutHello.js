b8af31e21d49a51a1d80f38eb354d2380d17543f
Nope <nope@example.com>
2020-06-18 11:35:56 -0500

    LayoutHello not happy with lorem ipsum text for header and footer

--- src/components/layoutHello.js
+++ src/components/layoutHello.js
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function LayoutHello({ children }) {
   return (
-    <div className="hello-layout-header">~ Hello, I'm lorem ipsum for a header.~</div>
+    <div className="hello-layout-header">Header lorem ipsum</div>
     <div className="hello-layout-main">
 	  { children }
     </div>
-	<div className="hello-layout-footer">~ Hello, I'm lorem ipsum for a footer.~</div>
+	<div className="hello-layout-footer">Footer lorem ipsum</div>
   )
 }
