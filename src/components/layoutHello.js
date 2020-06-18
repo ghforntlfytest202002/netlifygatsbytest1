6e95fd6cecf455a45391b884d1d49163d4081b77
Nope <nope@example.com>
2020-06-18 11:42:49 -0500

    Apparently JSX elements have to have a single root tag

--- src/components/layoutHello.js
+++ src/components/layoutHello.js
@@ -2,10 +2,12 @@ import React from "react"
 
 export default function LayoutHello({ children }) {
   return (
-    <div className="hello-layout-header">H lorem ipsum</div>
-    <div className="hello-layout-main">
-	  { children }
-    </div>
-	<div className="hello-layout-footer">F lorem ipsum</div>
+    <div className="hello-layout-wrapper">
+      <div className="hello-layout-header">H lorem ipsum</div>
+      <div className="hello-layout-main">
+	    { children }
+      </div>
+	  <div className="hello-layout-footer">F lorem ipsum</div>
+	</div>
   )
 }
