5b98e8edffe2ea906bd4ef6bf13d23dd2490e178
Nope <nope@example.com>
2020-06-18 16:15:54 -0500

    Typo

--- src/components/sectionTaskList.js
+++ src/components/sectionTaskList.js
@@ -5,7 +5,7 @@ import Task from './task.js';
 export default function SectionTaskList(props) {
   const alternatingClassNames = ['task-odd', 'task-even']; // Per https://stackoverflow.com/a/45467474
   const tasks = props.section.accomplishments;
-  const taskItems = tasks.map((taskToDo) =>
+  const taskItems = tasks.map((taskToDo, index) =>
 	<Task taskDetail={taskToDo} alternatingClassName={alternatingClassNames[index % alternatingClassNames.length]} />
   );
   return (
