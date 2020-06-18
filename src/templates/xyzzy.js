2e84e2fee7c547f6bdf8bc9e16bc6ce5f8cf349b
Nope <nope@example.com>
2020-06-18 07:38:51 -0500

    Rolling back to example 2

--- src/templates/xyzzy.js
+++ src/templates/xyzzy.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-import BasicDiv from '../components/basicDiv.js';
-
 export default function Xyzzy({ pageContext }) {
   return (
-    <BasicDiv message={pageContext.frontmatter.message} />
+    <div>
+	  {pageContext.frontmatter.message}
+    </div>
   )
 }
