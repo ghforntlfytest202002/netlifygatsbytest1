eb095974e43f9880f086c3bd7d2c4002e58894d2
Nope <nope@example.com>
2020-06-18 12:18:26 -0500

    I GOT IT!  Time for some CSS maybe.  First playing with fleshing out Task though

--- src/components/layoutHello.js
+++ src/components/layoutHello.js
@@ -3,11 +3,11 @@ import React from "react"
 export default function LayoutHello({ children }) {
   return (
     <div className="hello-layout-wrapper">
-      <div className="hello-layout-header">H lorem ipsum</div>
+      <div className="hello-layout-header">Header placeholder</div>
       <div className="hello-layout-main">
 	    { children }
       </div>
-	  <div className="hello-layout-footer">F lorem ipsum</div>
+	  <div className="hello-layout-footer">Footer placeholder</div>
 	</div>
   )
 }
--- src/components/task.js
+++ src/components/task.js
@@ -1,9 +1,22 @@
 import React from "react"
 
 export default function Task(props) {
+  let checkboxSymbol;
+  if (props.taskDetail.done) {
+	checkboxSymbol = {'✓'};
+  } else {
+	checkboxSymbol = {'X'}
+  }
   return (
     <div className="task">
-	  {props.taskDetail.how}
+	<b>{checkboxSymbol}</b>
+	{' '}
+	<b>{props.taskDetail.task}</b>
+	{ props.taskDetail.how.length > 0 && 
+	  <i>{' '}{props.taskDetail.how</i>
+	} {}      - task: eat
+        done: true
+        how: well{props.taskDetail.how}
     </div>
   )
 }
