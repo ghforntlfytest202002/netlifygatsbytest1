18be9782e6db39a8b299d35ea824fb7bead2d9bc
Nope <nope@example.com>
2020-06-19 16:40:48 -0500

    Trying TinaCMS editable SectionBlue

--- src/components/sectionBlue.js
+++ src/components/sectionBlue.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { liveRemarkForm } from 'gatsby-tinacms-remark';
+import { Wysiwyg } from '@tinacms/fields';
+import { TinaField } from '@tinacms/form-builder';
 
 export default function SectionBlue(props) {
   return (
     <div className="blue-div">
-	  {props.section.mention}
+	  <TinaField name="rawMarkdownBody" Component={Wysiwyg}>
+	    <article onClick={() => props.setIsEditing(true)}>
+	      {props.section.mention}
+	    </article>
+	  </TinaField>
     </div>
   )
 }
