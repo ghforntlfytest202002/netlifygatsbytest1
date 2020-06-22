d80e65b7df067e1ea72962f8a482e53696cae4d2
Nope <nope@example.com>
2020-06-22 16:30:07 -0500

    The last one built.  Yay.  It was at /index/ instead of / but I fixed that with a Sanity edit and redeploy. Now preparing to move to Gatsby Cloud playing with env variables.  Also, I could query a _id field in my gatsby-node and I could take off the non-null slug filter for, for example, the blog, I guess.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+const sanityWatchMode = process.env.SANITY_WATCH_MODE ? process.env.SANITY_WATCH_MODE : false;
+const sanityOverlayDrafts = process.env.SANITY_OVERLAY_DRAFTS ? process.env.SANITY_OVERLAY_DRAFTS : false;
+
 module.exports = {
     plugins: [
 		{
@@ -6,8 +9,8 @@ module.exports = {
 				projectId: "y16rns1e",
 				dataset: "my-content",
 				token: "sk8AepoFD3M6bYWdXZOEvFKvQtDpXAQK4NEegywpyvjEEB3ZbeEKkmFDlhifTDJ8H00Oxbd8aKUySeD7JA6CmpGgyMBrjXffxPI9t7wxbVYSGOxI2673vFqOuByOUsScyiGtg0d9AnNY1K82MZIHca8CIi15p4pXWuaeJ4aRPmXIp5m70UOR",
-				watchMode: false,
-				overlayDrafts: false,
+				watchMode: sanityWatchMode,
+				overlayDrafts: sanityOverlayDrafts,
 			}
 		}
     ]
