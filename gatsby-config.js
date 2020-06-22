29a018828ecddfbd8ce693647b8a42f54de092fc
Nope <nope@example.com>
2020-06-22 16:06:06 -0500

    OMG I had a typo in my Sanity project ID all this time

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
 		{
 			resolve: "gatsby-source-sanity",
 			options: {
-				projectId: "abc123",
+				projectId: "y16rns1e",
 				dataset: "my-content",
 				token: "sk8AepoFD3M6bYWdXZOEvFKvQtDpXAQK4NEegywpyvjEEB3ZbeEKkmFDlhifTDJ8H00Oxbd8aKUySeD7JA6CmpGgyMBrjXffxPI9t7wxbVYSGOxI2673vFqOuByOUsScyiGtg0d9AnNY1K82MZIHca8CIi15p4pXWuaeJ4aRPmXIp5m70UOR",
 				watchMode: false,
