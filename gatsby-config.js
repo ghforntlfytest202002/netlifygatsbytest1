5acd482a32a988a074116f47355dfd43c6c901e2
Nope <nope@example.com>
2020-06-22 16:47:54 -0500

    Seeing if I really defined tokens right

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 const sanityWatchMode = process.env.SANITY_WATCH_MODE ? process.env.SANITY_WATCH_MODE : false;
 const sanityOverlayDrafts = process.env.SANITY_OVERLAY_DRAFTS ? process.env.SANITY_OVERLAY_DRAFTS : false;
+const sanityToken = process.env.SANITY_READ_TOKEN ? process.env.SANITY_READ_TOKEN : "sk8AepoFD3M6bYWdXZOEvFKvQtDpXAQK4NEegywpyvjEEB3ZbeEKkmFDlhifTDJ8H00Oxbd8aKUySeD7JA6CmpGgyMBrjXffxPI9t7wxbVYSGOxI2673vFqOuByOUsScyiGtg0d9AnNY1K82MZIHca8CIi15p4pXWuaeJ4aRPmXIp5m70UOR";
 
 module.exports = {
     plugins: [
@@ -8,7 +9,7 @@ module.exports = {
 			options: {
 				projectId: "y16rns1e",
 				dataset: "my-content",
-				token: "sk8AepoFD3M6bYWdXZOEvFKvQtDpXAQK4NEegywpyvjEEB3ZbeEKkmFDlhifTDJ8H00Oxbd8aKUySeD7JA6CmpGgyMBrjXffxPI9t7wxbVYSGOxI2673vFqOuByOUsScyiGtg0d9AnNY1K82MZIHca8CIi15p4pXWuaeJ4aRPmXIp5m70UOR",
+				token: sanityToken,
 				watchMode: sanityWatchMode,
 				overlayDrafts: sanityOverlayDrafts,
 			}
