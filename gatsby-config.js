d5d11bbb726a33d0a69d8370176c4a0894c594f0
Nope <nope@example.com>
2020-06-22 15:07:22 -0500

    Switch to env variables soon but first just see if this works

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
 				projectId: "abc123",
 				dataset: "my-content",
 				token: "sk8AepoFD3M6bYWdXZOEvFKvQtDpXAQK4NEegywpyvjEEB3ZbeEKkmFDlhifTDJ8H00Oxbd8aKUySeD7JA6CmpGgyMBrjXffxPI9t7wxbVYSGOxI2673vFqOuByOUsScyiGtg0d9AnNY1K82MZIHca8CIi15p4pXWuaeJ4aRPmXIp5m70UOR",
-				watchMode: true,
-				overlayDrafts: true,
+				watchMode: false,
+				overlayDrafts: false,
 			}
 		},
     ]
