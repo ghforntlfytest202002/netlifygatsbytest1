ec2e74e5ccfbbc7aa2b00b1ddf5127046aa0aec2
Nope <nope@example.com>
2020-06-22 11:52:04 -0500

    Giving up on TinaCMS and Stackbit - back to v2 "message" baseline to play with Sanity instead

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,16 +7,6 @@ module.exports = {
                 path: `${__dirname}/src/pages`,
             },
         },
-		`gatsby-transformer-remark`,
-    		{
-			resolve: `gatsby-plugin-tinacms`,
-			options: {
-				sidebar: {
-					//hidden: process.env.NODE_ENV === "production",
-					position: "displace"
-				},
-				plugins: [`gatsby-tinacms-git`, `gatsby-tinacms-remark`]
-			}
-		}
-	]
+		`gatsby-transformer-remark`
+    ]
 };
--- src/templates/xyzzy.js
+++ src/templates/xyzzy.js
@@ -1,42 +1,9 @@
 import React from "react"
-import _ from 'lodash';
-import { remarkForm } from "gatsby-tinacms-remark";
 
-import LayoutHello from '../components/layoutHello.js';
-import sectionComponentTypeList from '../components/indexSectionComponents.js';
-
-function Xyzzy({ pageContext, data }) {
-  const sections = pageContext.frontmatter.sections;
-  const SectionComponents = sections.map((section) => {
-    let sectionType = _.upperFirst(_.camelCase(_.get(section, 'type')));
-	let Component = sectionComponentTypeList[sectionType];
-	return (
-	  <Component section={section} />
-	)
-  });
+export default function Xyzzy({ pageContext }) {
   return (
-    <LayoutHello>
-	  <div className='xyzzy'>
-	    {SectionComponents}
-	  </div>
-	  <div className='xyzzy-two'>
-	    {data.markdownRemark.frontmatter.miscfield}
-	  </div>
-	</LayoutHello>
+    <div>
+	  {pageContext.frontmatter.message}
+    </div>
   )
 }
-
-export const pageQuery = graphql`
-  query XyzzyById($suggestedURLSuffix: String!) {
-    markdownRemark(fields: { suggestedURLSuffix: { eq: $suggestedURLSuffix } }) {
-      id
-      frontmatter {
-        miscfield
-      }
-      ...TinaRemark
-    }
-  }
-`
-
-//export {Xyzzy as default};
-export default remarkForm(Xyzzy);
