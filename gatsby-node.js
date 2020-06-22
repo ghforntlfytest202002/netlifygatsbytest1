1faa2a80721d3ddb38180c882172636d7026818c
Nope <nope@example.com>
2020-06-22 16:08:55 -0500

    Gatsby Node console-logged a SanityXyzzy.  I think we may be in business.  Note that in all this troubleshooting, I did a sanity graphql deploy from my CLI.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,30 @@
-exports.onCreateNode = ({ node }) => {
-  console.log('Node Internal Type: ' + node.internal.type)
-}
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, getNode, actions }) => {
+  const { createPage } = actions
+  const queryResult = await graphql(`
+    query {
+      allSanityXyzzy(filter: {slug: {current: {ne: null}}}) {
+          edges {
+            node {
+              template,
+              message,
+              slug {
+                current
+              }
+            }
+          }
+      }
+    }
+  `)
+  nodes = queryResult.data.allSanityXyzzy.edges
+  nodes.forEach(({ node }) => {
+    createPage({
+      path: node.slug,
+      component: path.resolve(`./src/templates/${node.template}.js`),
+      context: {
+        message: node.message,
+      },
+    })
+  })
+};
