082bef94cd8770333b392c9a232650f5cc43bb39
Nope <nope@example.com>
2020-06-24 10:49:22 -0500

    Back to my v2-based Sanity experiment from v2-original

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,45 +1,30 @@
 const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const urlSuffixIdea = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `suggestedURLSuffix`,
-      value: urlSuffixIdea,
-    })
-  }
-}
-
-exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, getNode, actions }) => {
   const { createPage } = actions
   const queryResult = await graphql(`
-	query {
-	  allMarkdownRemark {
-		  edges {
-			node {
-			  fields {
-				suggestedURLSuffix
-			  },
-			  frontmatter {
-				template,
-				message
-			  }
-			}
-		  }
-	  }
-	}
+    query {
+      allSanityXyzzy(filter: {slug: {current: {ne: null}}}) {
+          edges {
+            node {
+              template,
+              message,
+              slug {
+                current
+              }
+            }
+          }
+      }
+    }
   `)
-  nodes = queryResult.data.allMarkdownRemark.edges
+  nodes = queryResult.data.allSanityXyzzy.edges
   nodes.forEach(({ node }) => {
     createPage({
-	  path: node.fields.suggestedURLSuffix,
-	  component: path.resolve(`./src/templates/${node.frontmatter.template}.js`),
-	  context: {
-	    frontmatter: node.frontmatter,
-	  },
+      path: node.slug.current,
+      component: path.resolve(`./src/templates/${node.template}.js`),
+      context: {
+        message: node.message,
+      },
     })
   })
 };
