c0a191e83a3904cf96c9d3b2889d385dde6ba81f
Nope <nope@example.com>
2020-07-08 10:18:20 -0500

    Attempt to use a slicker ES6-fancy gatsby config plugin options

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,13 @@
-const sanityWatchMode = process.env.SANITY_WATCH_MODE ? process.env.SANITY_WATCH_MODE : false;
-const sanityOverlayDrafts = process.env.SANITY_OVERLAY_DRAFTS ? process.env.SANITY_OVERLAY_DRAFTS : false;
-const sanityToken = process.env.SANITY_READ_TOKEN ? process.env.SANITY_READ_TOKEN : "sk8AepoFD3M6bYWdXZOEvFKvQtDpXAQK4NEegywpyvjEEB3ZbeEKkmFDlhifTDJ8H00Oxbd8aKUySeD7JA6CmpGgyMBrjXffxPI9t7wxbVYSGOxI2673vFqOuByOUsScyiGtg0d9AnNY1K82MZIHca8CIi15p4pXWuaeJ4aRPmXIp5m70UOR";
-
 module.exports = {
     plugins: [
 		{
 			resolve: "gatsby-source-sanity",
 			options: {
-				projectId: "y16rns1e",
+				projectId: "your-sanity-project-id",
 				dataset: "my-content",
-				token: sanityToken,
-				watchMode: sanityWatchMode,
-				overlayDrafts: sanityOverlayDrafts,
+				...(process.env.SANITY_READ_TOKEN && { token: process.env.SANITY_READ_TOKEN }),
+				...(process.env.SANITY_WATCH_MODE && process.env.SANITY_READ_TOKEN && { watchMode: process.env.SANITY_WATCH_MODE }),
+				...(process.env.SANITY_OVERLAY_DRAFTS && process.env.SANITY_READ_TOKEN && { overlayDrafts: process.env.SANITY_OVERLAY_DRAFTS }),
 			}
 		}
     ]
